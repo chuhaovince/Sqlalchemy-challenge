13cf56f21da8797d4dc34dc7e38562c46c1ba26b
chuhaovince <53956182+chuhaovince@users.noreply.github.com>
2019-12-02 15:03:32 -0500

    app.py and jupyter

--- app.py
+++ app.py
@@ -26,8 +26,41 @@ def homepage():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/start_end_date/<start>/<end><br/>"
+        f"/api/v1.0/start_date/20130101<br/>"
+        f"/api/v1.0/start_end_date/20130101/20160101<br/>"
     )
+
+@app.route('/api/v1.0/precipitation')
+def prcp():
+    session = Session(engine)
+    prcp_dict = {}
+    results = session.query(Measurement.date, Measurement.prcp).all()
+    for item in results:
+        # update the dict with date as the key and prcp as the value
+        prcp_dict[item[0]] = item[1]
+    session.close()
+    return jsonify(prcp_dict)
+
+@app.route('/api/v1.0/stations')
+def  stations():
+    session = Session(engine)
+    station = []
+    results = session.query(Station.station).group_by(Station.station).all()
+    for item in results:
+        station_dict = {}
+        station_dict["station"] = item
+        # create the stations as a list of dictionaries
+        # or just do 
+        # station += item    to get all the unique stations as a list
+        station.append(station_dict)
+    session.close()
+    return jsonify(station)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+    results = session.query(Measurement.)
+    session.close()
 
 if __name__ == "__main__":
     app.run(debug=True)
